7bfab7c00512e1cd46f3b684ec426134c488a29f
Zelo101 <umadeguy@gmail.com>
2025-03-08 02:12:32 +0000

    Keybind to change render settings

--- neoforge/src/main/java/dev/zelo/renderscale/RenderScale.java
+++ neoforge/src/main/java/dev/zelo/renderscale/RenderScale.java
@@ -2,21 +2,28 @@ package dev.zelo.renderscale;
 
 import dev.zelo.renderscale.config.RenderScaleConfig;
 import me.shedaniel.autoconfig.AutoConfig;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.AddReloadListenerEvent;
+import org.lwjgl.glfw.GLFW;
 
 @Mod(value = Constants.MOD_ID, dist = Dist.CLIENT)
 public class RenderScale {
     private static final Minecraft client = Minecraft.getInstance();
+
+    // TODO: Consider using Lazy? (https://docs.neoforged.net/docs/misc/keymappings/#checking-a-keymapping)
+    private static final KeyMapping keyBinding = new KeyMapping("key.renderscale.options", GLFW.GLFW_KEY_O, "key.renderscale.category");
 
     public RenderScale(IEventBus eventBus, ModContainer modContainer) {
         CommonClass.init();
@@ -42,10 +49,19 @@ public class RenderScale {
         if (client.level == null && CommonClass.getInstance().hasRun) {
             CommonClass.getInstance().hasRun = false;
         }
+
+        while (keyBinding.consumeClick()) {
+            client.setScreen(AutoConfig.getConfigScreen(RenderScaleConfig.class, client.screen).get());
+        }
     }
 
     public void onDatapackReload(AddReloadListenerEvent event) {
         AutoConfig.getConfigHolder(RenderScaleConfig.class).load();
+    }
+
+    @SubscribeEvent
+    public void registerBindings(RegisterKeyMappingsEvent event) {
+        event.register(keyBinding);
     }
 }
 
