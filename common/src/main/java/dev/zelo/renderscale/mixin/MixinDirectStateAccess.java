1250e5888d04126efda6e1bb4f9ea1b3cfd3ac49
Zelo101 <umadeguy@gmail.com>
2025-06-13 22:23:33 +0100

    Better scale compat (Fix #29)

---
+++ common/src/main/java/dev/zelo/renderscale/mixin/MixinDirectStateAccess.java
@@ -0,0 +1,34 @@
+package dev.zelo.renderscale.mixin;
+
+import com.mojang.blaze3d.opengl.DirectStateAccess;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(DirectStateAccess.class)
+public interface MixinDirectStateAccess {
+    @Invoker("blitFrameBuffers")
+    void invokeBlitFrameBuffers(
+            int readFrameBuffer,
+            int drawFrameBuffer,
+            int srcX0,
+            int srcY0,
+            int srcX1,
+            int srcY1,
+            int destX0,
+            int destY0,
+            int destX1,
+            int destY1,
+            int mask,
+            int filter
+    );
+
+//    @Invoker("bindFrameBufferTextures")
+//    void invokeBindFrameBufferTextures(
+//            int frameBuffer,
+//            int colorTexture,
+//            int depthTexture,
+//            int level,
+//            int target,
+//            boolean useStencil
+//    );
+}
